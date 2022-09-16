010e7222b191552f65f6917dcbd1998e428e4df0
Antoine Metifeu <ametifeu@gmail.com>
2022-09-16 09:17:06 +0200

    refactor: event publisher

--- src/infrastructure/event-publisher.ts
+++ src/infrastructure/event-publisher.ts
@@ -3,31 +3,31 @@ import { type handler, IEventPublisher } from './event-publisher.interface';
 import { IEventStore } from './event-store.interface';
 
 export class EventPublisher implements IEventPublisher {
-  #subscribers = new Map<string, handler[]>();
+  #handlers = new Map<string, handler[]>();
 
-  constructor(private readonly store: IEventStore) {}
+  constructor(private readonly eventStore: IEventStore) {}
 
   public publish(events: IEvent[]) {
-    this.store.save(events);
+    this.eventStore.save(events);
 
     events.forEach((event) => {
       const eventType = event.constructor.name;
 
-      if (!this.#subscribers.has(eventType)) {
+      if (!this.#handlers.has(eventType)) {
         return;
       }
 
-      this.#subscribers.get(eventType).forEach((handler) => {
+      this.#handlers.get(eventType).forEach((handler) => {
         handler(event);
       });
     });
   }
 
   public subscribe(eventType: string, cb: handler) {
-    if (!this.#subscribers.has(eventType)) {
-      this.#subscribers.set(eventType, []);
+    if (!this.#handlers.has(eventType)) {
+      this.#handlers.set(eventType, []);
     }
 
-    this.#subscribers.get(eventType).push(cb);
+    this.#handlers.get(eventType).push(cb);
   }
 }
